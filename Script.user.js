0370fcb1d57873b5af25af4640c66aa8dc104a3e
MXPSQL Server 20953 onetechguy <73059135+MXP2095onetechguy@users.noreply.github.com>
2021-06-06 14:49:02 +0700

    Update Script.user.js

--- Script.user.js
+++ Script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Shutterstock Downloader for tampermonke(or tapLmonke)
 // @namespace    MXPServer 20953 Onetechguy
-// @version      2.9
+// @version      2.10
 // @description  Shutterstock downloader script
 // @author       MXPServer 20953 Onetechguy
 // @include      https://www.shutterstock.com/image-photo/*
@@ -26,7 +26,7 @@ function addJQuery(callback) {
 
     // Your code here...
     //links: href="https://downloader.la/Ext.php?url=' + window.location.href + '" target="_blank
-    //tags: <a id="downloadBtnCodeCyan" href="https://downloader.la/Ext.php?url=' + window.location.href + '" target="_blank">hehe</a>
+    //tags: <a id="downloadBtnCodeCyan" href="https://downloader.la/Ch.php?url=' + window.location.href + '" target="_blank">hehe</a>
     //div: m_l_5f920
     /*
     var hehe = document.createElement('a');
@@ -36,7 +36,7 @@ function addJQuery(callback) {
     hehe.title = "Download!";
     hehe.href = "https://downloader.la/Ext.php?url=" + window.location.href;
     div.appendChild(hehe);*/
-    var finalLink = "https://downloader.la/Ext.php?url=" + window.location.href;
+    var finalLink = "https://downloader.la/Ch.php?url=" + window.location.href;
     /* ---------------- Style  --------------- */
     var style = document.createElement('style');
     style.type = 'text/css';
@@ -56,7 +56,7 @@ function addJQuery(callback) {
     */
     gt_btn.innerHTML='<a target="_blank" href=' + finalLink + '> Download Image <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></a><!--<button class="btn-primary" onclick="$( "#LinkHAHAisCreatortheMXPServer20953Onetechguy" ).remove()">remove</button>-->';
     more_btn.innerHTML='<a target="_blank" href='+ 'https://downloader.la'+'> More Utils at Downloader.la </a><!--<button class="btn-primary" onclick="$( "#LinkUTILSisCreatortheMXPServer20953Onetechguy" ).remove()">remove</button>-->';
-    exp_btn.innerHTML = '<a target="_blank" href="https://downloader.la/Ext.php?url=https://www.shutterstock.com/image-vector/business-woman-slipping-falling-banana-peel-308325830">Example<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></a>';
+    exp_btn.innerHTML = '<a target="_blank" href="https://downloader.la/Ch.php?url=https://www.shutterstock.com/image-vector/business-woman-slipping-falling-banana-peel-308325830">Example<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></a>';
     document.querySelector('body').appendChild(gt_btn);
     document.querySelector('body').appendChild(more_btn);
 
